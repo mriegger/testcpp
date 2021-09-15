b72b7b6003259f6f51aa8081d0c3d3ae6398169f
unknown <Michael Riegger>
2021-09-15 09:20:54 -0700

    Adding upperbound

--- BinarySearch.h
+++ BinarySearch.h
@@ -17,8 +17,8 @@ public:
 		while (left <= right)
 		{
 			const T mid = left + (right - left) / 2;
-			const bool comparesGreater = v[mid] < target;
-			if (comparesGreater) 
+			const bool targetGreaterMid = v[mid] < target;
+			if (targetGreaterMid)
 			{
 				left = mid + 1;
 			}
@@ -31,12 +31,37 @@ public:
 		return v.begin() + best;
 	}
 
+	template<typename T>
+	inline static std::vector<T>::const_iterator UpperBound(const std::vector<T>& v, const T target)
+	{
+		int left = 0;
+		int right = v.size() - 1;
+		int best = v.size();
+
+		while (left <= right)
+		{
+			const T mid = left + (right - left) / 2;
+			const bool targetLessMid = target < v[mid];
+			if (targetLessMid)
+			{
+				best = mid;
+				right = mid - 1;
+			}
+			else
+			{
+				left = mid + 1;
+			}
+		}
+		return v.begin() + best;
+	}
+
 	static void Test()
 	{
 		const std::vector<int> v = { 1,4,6,8 };
 		for (int i = -10; i <= 10; ++i)
 		{
 			assert(lower_bound(v.begin(), v.end(), i) == LowerBound(v, i));
+			assert(upper_bound(v.begin(), v.end(), i) == UpperBound(v, i));
 		}
 	}
 
