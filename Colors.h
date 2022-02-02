648d51f643d46a982e35996cac62e6778f664143
Michael Riegger <mikeriegger@gmail.com>
2022-02-02 09:30:20 -0800

    more colors

--- Colors.h
+++ Colors.h
@@ -11,4 +11,9 @@ namespace Colors
 	static constexpr float3 Yellow(1, 1, 0);
 	static constexpr float3 Cyan(0, 1, 1);
 	static constexpr float3 Purple(0.5f, 0, 0.5f);
+	static constexpr float3 Maroon(0.5f, 0, 0);
+	static constexpr float3 Teal(0, 0.5f, 0.5f);
+	static constexpr float3 Gray(0.5f, 0.5f, 0.5f);
+	static constexpr float3 Silver(0.75f, 0.75f, 0.75f);
+	static constexpr float3 Navy(0, 0, 0.5f);
 }
